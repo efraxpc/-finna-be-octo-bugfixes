bc3e95ab1fb8a23cc197f7a82dcbe813e3a82dee
efraxpc <efraxpc@gmail.com>
2015-07-15 00:19:16 -0700

    paginacion manual works fine

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -16,7 +16,20 @@ class HomeController extends BaseController {
 	*/
 
 	public function testGet(){
-        return Test::paginate(10);
+        //return Test::paginate(10);
+        $oTest = new Test();
+        $oElementosPaginacion = $oTest->Obtener_elementos_paginacion();
+        // Get pagination information and slice the results.
+        $perPage = 4;
+        $total = count($oElementosPaginacion);
+        $start = (Paginator::getCurrentPage() - 1) * $perPage;
+        $sliced = array_slice($oElementosPaginacion, $start, $perPage);
+
+        // Eager load the relation.
+        $collection = Test::hydrate($sliced);
+
+        // Create a paginator instance.
+        return Paginator::make($collection->all(), $total, $perPage);        
 	}
     
     public function llenar_tabla_test(){
--- app/models/Test.php
+++ app/models/Test.php
@@ -1,4 +1,9 @@
 <?php 
 class Test extends Eloquent{
     protected $table = 'test';
+
+    public function Obtener_elementos_paginacion()
+    {
+        return DB::select('CALL test_Obtener_elementos_paginacion()');
+    }
 }
